c50acc89f092fb4e292fabd6eb1cf2c1e75af2bf
Maryna Sibirtsava <s24451@pjwstk.edu.pl>
2023-06-10 21:11:14 +0200

    dateUpdate

--- GUI.py
+++ GUI.py
@@ -36,7 +36,7 @@ class GUI:
                                         font=("Helvetica", 10))
         self.end_date_entry.grid(row=4, column=1, padx=10)
 
-        self.process_button = tk.Button(self.window, text="Process Data", command=self.handle_process_button,
+        self.process_button = tk.Button(self.window, text="Process Data", command=self.update_graph,
                                         bg="light blue")
         self.process_button.grid(row=5, column=0, columnspan=2, padx=10, pady=20)
 
@@ -50,7 +50,7 @@ class GUI:
         self.bank_var1 = tk.BooleanVar()
         self.bank_var2 = tk.BooleanVar()
 
-        self.bank_label = tk.Label(self.window, text="Select Bank(s):", bg="white", font=("Helvetica", 10))
+        self.bank_label = tk.Label(self.window, text="Select Company(s):", bg="white", font=("Helvetica", 10))
         self.bank_label.grid(row=0, column=0, padx=10, pady=10)
 
         self.bank_check1 = ttk.Checkbutton(self.window, text="Microsoft", variable=self.bank_var1)
@@ -58,6 +58,7 @@ class GUI:
 
         self.bank_check2 = ttk.Checkbutton(self.window, text="Apple", variable=self.bank_var2)
         self.bank_check2.grid(row=1, column=1, padx=10)
+
 
     def handle_process_button(self):
         start_date = self.start_date_entry.get_date().strftime("%Y-%m-%d")
@@ -80,10 +81,10 @@ class GUI:
         downloader = DataDownloader(banks, start_date, end_date)
         downloader.download_data()
         data_list = []
-        
+
         for bank in banks:
             filename = os.path.join(os.getcwd(), bank.replace(" ", "_"),
-                                f"{bank.replace(' ', '_')}_data_{start_date}_to_{end_date}.csv")
+                            f"{bank.replace(' ', '_')}_data_{start_date}_to_{end_date}.csv")
             data = pd.read_csv(filename)
             cleaned_data = downloader.clean_data(data)
             cleaned_data['Company'] = bank  # Add a column for company name
@@ -93,9 +94,19 @@ class GUI:
 
         self.result_label.config(text=f"Selected dates: {start_date} - {end_date}, Bank(s): {', '.join(banks)}")
 
+    def update_graph(self):
+    # Clear the plot frame
+        self.plot_frame.destroy()
+        self.plot_frame = tk.Frame(self.window, bg="white")
+        self.plot_frame.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+
+        self.handle_process_button()
+
+
 
     def plot_data(self, data_list):
         figure, ax = plt.subplots(figsize=(6, 4), dpi=100)
+        ax.clear()
 
         for data in data_list:
             ax.plot(data['Date'], data['Close'], label=data['Company'][0])
